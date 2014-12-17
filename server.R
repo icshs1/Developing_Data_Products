ccff0348a08c0aeb09f8e64ae41136d767042fc6
Hyunsik Shim <icshs1@naver.com>
2014-12-17 19:51:05 +0900

    3rd upgrade

--- server.R
+++ server.R
@@ -10,6 +10,35 @@ shinyServer(
     down_data<-reactive({ 
       subset(mtcars,select=input$id3) 
     }) 
+    # select predictor 
+    predictor <-reactive({input$id4}) 
+    
+    # adjust value according to selected predictor 
+    predict_value <-reactive({ 
+      if(input$id4=="disp") 
+        predict_value<-input$id5 
+      else if(input$id4=="hp") 
+        predict_value<-input$id6 
+      if(input$id4=="wt") 
+        predict_value<-input$id7 
+      if(input$id4=="qsec") 
+        predict_value<-input$id8 
+      return(predict_value) 
+    }) 
+    
+    # linear model training 
+    # predictor am and selected variable (hp, dist, qsec, wt) 
+    ## fit<- lm(mpg ~ x1 + am + x1 * am, mtcars) 
+    fit<- reactive({ 
+      fit<-lm(mpg ~ mtcars[ , predictor()] + am + mtcars[ , predictor()]* am, mtcars) 
+      return(fit) 
+    }) 
+    # predict resutl with input predict value. 
+    predict_result<- reactive({ 
+      predict<-c(fit()$coef[1] + predict_value() * fit()$coef[2], 
+                 fit()$coef[1] + fit()$coef[3] + predict_value() * (fit()$coef[2] + fit()$coef[4])) 
+      return(predict) 
+    }) 
     
     output$plot <- renderPlot({ 
       
@@ -18,25 +47,48 @@ shinyServer(
       w=mtcars[ , value()] 
       hist(w,main = title,xlab=xlab) 
     }) 
-     
+    
     
     output$plot_corr <- renderPlot({ ## plot correlation between variables 
       pairs(select_data()) 
     }) 
     
+    
     output$table <- renderDataTable( ## save data using selected variables 
-{down_data()}, options = list(bFilter = FALSE, iDisplayLength = 10)) 
+      {down_data()}, options = list(bFilter = FALSE, iDisplayLength = 10)) 
 
-output$downloadData <- downloadHandler( 
-  filename = function() { 
-    paste('data-', Sys.Date(), '.csv', sep='') 
-  }, 
-  content = function(file) { 
-    write.csv(down_data(), file, row.names=FALSE) 
+    output$downloadData <- downloadHandler( 
+      filename = function() { 
+        paste('data-', Sys.Date(), '.csv', sep='') 
+      }, 
+      content = function(file) { 
+        write.csv(down_data(), file, row.names=FALSE) 
+      } 
+    ) 
+
+    output$plot_modeling <- renderPlot({ 
+  
+      title <- "Linear Model of mtcars" 
+  
+      ## plot mpg and selected predictor 
+      x1<- mtcars[ , predictor()] 
+      plot(x=x1,y=mtcars$mpg,col = (mtcars$am == 0)*2 + 2,main=title,xlab=predictor(),ylab="mpg") 
+  
+      # plot model coefs 
+      abline(c(fit()$coef[1],fit()$coef[2]),col='blue') 
+      abline(c(fit()$coef[1] + fit()$coef[3],fit()$coef[2] + fit()$coef[4]),col='red') 
+  
+      # plot results of prediction 
+      input$goButton 
+      isolate(points(x=predict_value(),y=predict_result()[1], col='blue', pch = 17)) 
+      input$goButton 
+      isolate(points(x=predict_value(),y=predict_result()[2], col='red', pch = 17)) 
+    }) 
+
+    ## print results of prediction 
+    output$predictedValue <- renderText({input$goButton 
+                             isolate(sprintf("For %s %g\n automatic is: %g mpg\n standard is %g mpg", 
+                             input$id4, predict_value(), predict_result()[1], predict_result()[2])) 
+    }) 
   } 
-) 
-## DEBUG 
-output$text_output <- renderText({sprintf("%s %s",input$id1,input$id2) 
-}) 
-  } 
-) 
+)
--- ui.R
+++ ui.R
@@ -1,35 +1,67 @@
-library(UsingR)
-data(mtcars)
-variables <- sort(colnames(mtcars))
-shinyUI(
-  navbarPage("mtcars Database Explorer",
-             tabPanel("Historam",
-                      sidebarPanel(
+library(UsingR) 
+data(mtcars) 
+variables <- sort(colnames(mtcars)) 
+
+predictorRange <<- list(c(min(mtcars$disp),max(mtcars$disp)), 
+                        c(min(mtcars$hp),max(mtcars$hp)), 
+                        c(min(mtcars$wt),max(mtcars$wt)), 
+                        c(min(mtcars$qsec),max(mtcars$qsec))) 
+shinyUI( 
+  navbarPage("mtcars Database Explorer", 
+             tabPanel("Historam", 
+                      sidebarPanel( 
                         radioButtons("id1", "Choose variables", variables ) 
-                      ),
-                      mainPanel(
+                      ), 
+                      mainPanel( 
                         plotOutput('plot') 
-                      )
-             ),
-             tabPanel("Correlation",
-                      sidebarPanel(
+                      ) 
+             ), 
+             tabPanel("Correlation", 
+                      sidebarPanel( 
                         checkboxGroupInput("id2", "Select Varaiables", variables, selected=variables) 
-                      ),
-                      mainPanel(
+                      ), 
+                      mainPanel( 
                         plotOutput('plot_corr') 
-                      )
-             ),
-             tabPanel("Download",
-                      sidebarPanel(
+                      ) 
+             ), 
+             tabPanel("Download", 
+                      sidebarPanel( 
                         checkboxGroupInput("id3", "Select Varaiables", variables, selected=variables) 
-                      ),
-                      mainPanel(
-                        dataTableOutput(outputId="table"),
+                      ), 
+                      mainPanel( 
+                        dataTableOutput(outputId="table"), 
                         downloadButton('downloadData', 'Download') 
-                      )
-             ),
-             tabPanel("About",
-                      mainPanel(includeMarkdown("introduction.md"))
-             )
+                      ) 
+             ), 
+             tabPanel("Linear Model", 
+                      sidebarPanel( 
+                        radioButtons("id4", "Choose variables", c("disp","hp","wt","qsec")), 
+                        numericInput("id5","disp Predictor value",(predictorRange[[1]][1] + predictorRange[[1]][2])/2.0, 
+                                     min=predictorRange[[1]][1], 
+                                     max=predictorRange[[1]][2], 
+                                     step=0.0000001), 
+                        numericInput("id6","hp Predictor value",(predictorRange[[2]][1] + predictorRange[[2]][2])/2.0, 
+                                     min=predictorRange[[2]][1], 
+                                     max=predictorRange[[2]][2], 
+                                     step=0.0000001), 
+                        numericInput("id7","wt Predictor value",(predictorRange[[3]][1] + predictorRange[[3]][2])/2.0, 
+                                     min=predictorRange[[3]][1], 
+                                     max=predictorRange[[3]][2], 
+                                     step=0.0000001), 
+                        numericInput("id8","qsec Predictor value",(predictorRange[[4]][1] + predictorRange[[4]][2])/2.0, 
+                                     min=predictorRange[[4]][1], 
+                                     max=predictorRange[[4]][2], 
+                                     step=0.0000001), 
+                        actionButton('goButton','Go!') 
+                      ), 
+                      mainPanel( 
+                        plotOutput('plot_modeling'), 
+                        h4('Predicted Milage values:'), 
+                        verbatimTextOutput("predictedValue") 
+                      ) 
+             ), 
+             tabPanel("About", 
+                      mainPanel(includeMarkdown("introduction.md")) 
+             ) 
   ) 
 )
